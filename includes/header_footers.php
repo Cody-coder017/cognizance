64e22c90d97b13ab8cdccfa07e893b09c5814a85
jgreig <josh.greig@gmail.com>
2018-04-02 06:48:45 -0400

    added a brain favicon

--- includes/header_footers.php
+++ includes/header_footers.php
@@ -8,6 +8,8 @@ function printHeader($page_class_name, $scripts = [])
 		<meta charset="utf-8"> 
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
 		<title>Cognizance</title>
+		<!--[if IE]><link rel="shortcut icon" href="images/favicon.ico"><![endif]-->
+		<link rel="icon" href="images/favicon.png">
 		<meta name="author" content="Josh Greig">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<script src="js/jquery-3.3.1.min.js"></script>
