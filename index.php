71e4ef96d77f737181cd076caadba7aff47555f4
jgreig <josh.greig@gmail.com>
2018-04-02 06:47:33 -0400

    index.php: improved appearance of the index page

--- index.php
+++ index.php
@@ -2,15 +2,20 @@
 require_once('includes/header_footers.php');
 printHeader('index');
 ?>	<h1>Cognizance</h1>
+	<p>Rate your cognitive performance</p>
 	<div class="links">
 		<div>
+			<a href="welcome.php">
 			<h2>New here?</h2>
-			<a href="welcome.php">Get Started.</a>
+			<span>Get Started.</span>
+			</a>
 		</div>
 		
 		<div>
+			<a href="signin.php">
 			<h2>Already have an account?</h2>
-			<a href="signin.php">Sign in.</a>
+			<span>Sign in.</span>
+			</a>
 		</div>
 	</div>
 <?php
