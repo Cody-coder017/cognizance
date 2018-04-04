672b3cb1b98d9de7956dc77ab338028a8f6a49b7
jgreig <josh.greig@gmail.com>
2018-04-03 21:20:53 -0400

    test2: added random shapes and bounced the circle around

--- js/test2.js
+++ js/test2.js
@@ -1,58 +1,91 @@
 $(document).ready(function() {
-	var v = {'x': 0, 'y': 0};
-	var p = {'x': 0, 'y': 0};
-	var r = 10;
 	
-	function normalize(v, scale, max_change) {
-		var r = Math.sqrt(v.x * v.x + v.y * v.y);
-		if ( r < 0.0001 ) {
-			return;
+	var draw = SVG('svg-display').size('100%', '100%');
+	var circleDiameter = getAveragePixelDimension() * 0.1;
+	var circle = draw.circle(circleDiameter, circleDiameter).fill('#fff');
+	var d = [getMaxX() / 2, getMaxY() / 2];
+	var v = [0, 0];
+	var randomShapeQueue = [];
+	var maxRandomShapes = 5;
+	var ballVelocityVariation = getAveragePixelDimension() * 0.002;
+	
+	function randomizeVelocity() {
+		v[0] += (Math.random() - 0.5) * ballVelocityVariation;
+		v[1] += (Math.random() - 0.5) * ballVelocityVariation;
+	}
+	
+	function getAveragePixelDimension() {
+		return (getMaxX() + getMaxY()) / 2;
+	}
+	
+	function getMaxX() {
+		var result = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+		return result;
+	}
+	
+	function getMaxY() {
+		var result = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+		return result;
+	}
+
+	function processBoundaryCollision() {
+		if( v[0] + d[0] < 0 ) {
+			v[0] = Math.abs(v[0]);
 		}
-		var change_amount = Math.min(r / scale * max_change, max_change);
-		var ax = v.x / r;
-		var ay = v.y / r;
-		r = Math.max(change_amount, r - change_amount);
-		v.x = ax * r;
-		v.y = ay * r;
+		else if( v[0] + d[0] > getMaxX() - circleDiameter ) {
+			v[0] = -Math.abs(v[0]);
+		}
+		if( v[1] + d[1] < 0 ) {
+			v[1] = Math.abs(v[1]);
+		}
+		else if( v[1] + d[1] > getMaxY() - circleDiameter ) {
+			v[1] = -Math.abs(v[1]);
+		}
+		return false;
+	}
+
+	function processInterval() {
+		if (!processBoundaryCollision()) {
+			circle.x(d[0]);
+			circle.y(d[1]);
+			console.log(JSON.stringify(d));
+		}
 	}
 	
-	function limitPosition() {
-		var radius = 10;
-		p.x = Math.max(-50 + radius, p.x);
-		p.x = Math.min(50 - radius, p.x);
-		p.y = Math.max(-50 + radius, p.y);
-		p.y = Math.min(50 - radius, p.y);
-	}
-
-	function moveCircle() {
-		p.x += v.x;
-		p.y += v.y;
-		
-		var new_position = {
-			'cx': (p.x + 50) + '%',
-			'cy': (p.y + 50) + '%',
-			'r': r + '%'
-		};
-
-		v.x += Math.random() - 0.5;
-		v.y += Math.random() - 0.5;
-		r = Math.min(20, Math.max(0.1, r + Math.random() - 0.5));
-		normalize(v, 5, 0.5);
-		limitPosition();
-
-		$('#followed-circle').css(new_position);
+	function getRandomColour() {
+		return '#' + Math.random().toString(16).substring(2, 5);
 	}
 
 	function addRandomShape() {
+		var size = getAveragePixelDimension() * 0.05 * (1 + Math.random());
+		var x = Math.random() * (getMaxX() - size);
+		var y = Math.random() * (getMaxY() - size);
+
+		if (randomShapeQueue.length > maxRandomShapes) {
+			randomShapeQueue[0].remove();
+			randomShapeQueue.shift(); // remove the first from the queue.
+		}
+		var newShape;
+		if (Math.random() < 0.5)
+			newShape = draw.rect(size, size);
+		else {
+			newShape = draw.circle(size, size);
+		}
 		
+		newShape.move(x, y);
+		randomShapeQueue.push(newShape);
+		newShape.animate().fill(getRandomColour());
 	}
-	
-	function animationUpdated() {
-		moveCircle();
-		if ( Math.random() < 0.01 ) {
+
+	function moveBox() {
+		processInterval();
+		d[0] += v[0];
+		d[1] += v[1];
+		randomizeVelocity();
+		if (Math.random() < 0.05) {
 			addRandomShape();
 		}
 	}
-	
-	setInterval(animationUpdated, 50);
+
+	window.setInterval(moveBox, 20);
 });
--- test2.php
+++ test2.php
@@ -1,13 +1,9 @@
 <?php
 require_once('includes/header_footers.php');
 
-printHeader('test2 test', ['js/test2.js']);
+printHeader('test2 test', ['js/svg.js', 'js/test2.js']);
 ?>
-<div class="content">
-	<svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-		<rect id="background"></rect>
-		<circle id="followed-circle" />
-	</svg>
+<div class="content" id="svg-display">
 </div>
 <?php
 
