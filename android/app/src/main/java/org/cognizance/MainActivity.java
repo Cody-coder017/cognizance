91b19377d9625aabe688bce890823d94c00ef02d
jgreig <josh.greig@gmail.com>
2018-04-04 22:26:48 -0400

    android: started Android application

---
+++ android/app/src/main/java/org/cognizance/MainActivity.java
@@ -0,0 +1,17 @@
+package org.cognizance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webview = new WebView(this);
+        setContentView(webview);
+        webview.loadUrl("http://polla.bhstudents.net/cognizance/");
+    }
+}
