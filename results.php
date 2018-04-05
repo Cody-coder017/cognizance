0273ce469def1aba9057537a50fb446556e28878
jgreig <josh.greig@gmail.com>
2018-04-04 23:05:26 -0400

    results: started page for showing results

---
+++ results.php
@@ -0,0 +1,19 @@
+<?php
+require_once('includes/header_footers.php');
+printHeader('results', []);
+?>
+	<div class="content">
+		<h1>My Results</h1>
+		
+		<div class="rating-name">Slightly Impaired</div>
+		
+		<h2 class="rating-number">3.0</h2>
+		
+		<p class="rating-description">
+		Some cognitive impairment, Decreased score results.
+		Can attend work and operate machinery.
+		</p>
+	</div>
+<?php
+
+printFooter();
