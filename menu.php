dacd8207b6e11fb96f896c9c6292c3bf74c45384
jgreig <josh.greig@gmail.com>
2018-04-11 19:29:35 -0400

    menu: started a page for navigation
    Related to #13

---
+++ menu.php
@@ -0,0 +1,17 @@
+<?php
+require_once('includes/header_footers.php');
+
+printHeader('menu', []);
+?>
+	<div class="content">
+		<h1>Menu</h1>
+		<ul>
+			<li>My Personal Profile</li>
+			<li><a href="test1_directions.php">Instructions</a></li>
+			<li>Tests</li>
+			<li><a href="results.php">My Results</a></li>
+		</ul>
+	</div>
+<?php
+
+printFooter();
