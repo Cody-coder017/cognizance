2386d02c9e3906f956a9b2835ce701a543694db1
jgreig <josh.greig@gmail.com>
2018-04-21 10:54:33 -0400

    android: hid app name toolbar and made navigation work within the WebView
    This involved making the application's WebView fill the screen by
    removing the application name 'Cognizance' from showing in the toolbar.
    
    Clicking a link caused another browser to open the link.  This commit
    changes it so the link opens in the same WebView control.

--- android/app/src/main/java/org/cognizance/MainActivity.java
+++ android/app/src/main/java/org/cognizance/MainActivity.java
@@ -1,17 +1,34 @@
 package org.cognizance;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private static void openLinksInWebView(WebView webview) {
+        webview.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                view.loadUrl(request.getUrl().toString());
+                return false;
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         WebView webview = new WebView(this);
+        openLinksInWebView(webview);
         setContentView(webview);
+        WebSettings webSettings = webview.getSettings();
+        webSettings.setJavaScriptEnabled(true);
         webview.loadUrl("http://polla.bhstudents.net/cognizance/");
     }
 }
